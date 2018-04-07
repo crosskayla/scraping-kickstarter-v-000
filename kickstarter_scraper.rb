345eaf062d668764d1c6208f931184934e5136e5
Kayla Cross <kac288@georgetown.edu>
2018-04-07 22:33:07 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,7 @@ def create_project_hash
   projects = {}
   
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = project
+    projects[project.css("h2.bbcard_name strong a").text] = project
   end
   
   projects
